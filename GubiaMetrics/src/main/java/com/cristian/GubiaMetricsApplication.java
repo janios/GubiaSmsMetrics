d9eeaba1d58ea9152479dac159075e25654c044b
Cristian Escamilla - c0e027c <escamilla.cristian@gmail.com>
2021-08-16 17:55:39 -0500

    Initial Commit

---
+++ GubiaMetrics/src/main/java/com/cristian/GubiaMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.cristian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GubiaMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GubiaMetricsApplication.class, args);
+	}
+
+}
